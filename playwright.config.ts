10a41ae7dcd0a4b9205803a97f0f4781262df54d
kurowskik <k.kurowski@futuremind.pl>
2026-03-08 23:16:30 +0100

    config: add timeout and set baseURL to try.playwright.tech

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,13 +2,14 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  timeout: 10 * 1000,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: [['html', { open: 'never' }]],
   use: {
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://try.playwright.tech/',
     trace: 'on',
   },
 
